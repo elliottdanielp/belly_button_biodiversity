f42702a8e711e130530cae16f95e1e070a7dc82e
Dan <106495422+elliottdanielp@users.noreply.github.com>
2022-08-29 12:00:33 -0400

    update charts.js

--- charts.js
+++ charts.js
@@ -110,7 +110,35 @@ function buildCharts(sample) {
       }]
     };
     // 10. Use Plotly to plot the data with the layout.
-    Plotly.newPlot("bar", barData, barLayout, {responsive: true}); 
+    Plotly.newPlot("bar", barData, barLayout, {responsive: true});
+    
+    // Bubble charts
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [{
+      x: otuIds,
+      y: sampleValues,
+      text: otuLabels,
+      mode: 'markers',
+      marker: {
+        size: sampleValues,
+        color: otuIds,
+        colorscale: "Earth"
+      }
+    }];
+    console.log(bubbleData);
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: 'Bacteria Cultures Per Sample',
+      showlegend: false,
+      xaxis: {title: "OTU ID", automargin: true},
+      yaxis: {automargin: true},
+      //margin: { t: 50, r: 50, l: 50, b: 50 },
+      hovermode: "closest"
+    };
+    console.log(bubbleLayout);
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout, {responsive: true});
     
   });
 }
